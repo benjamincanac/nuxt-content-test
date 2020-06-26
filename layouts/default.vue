7da89e31ee49d5dbac2ed3e6c2f096a555ac4b9f
Benjamin Canac <canacb1@gmail.com>
2020-06-26 17:52:40 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="container mx-auto px-4 lg:px-8">
+    <div class="flex flex-wrap relative h-screen">
+      <aside class="w-full lg:w-1/5 py-4 lg:p-8 lg:border-r">
+        <!-- TODO: Categories -->
+        <ul>
+          <li>...</li>
+        </ul>
+      </aside>
+
+      <Nuxt class="w-full lg:w-4/5" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/_slug.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-wrap-reverse">
+    <div class="w-full lg:w-3/4 py-4 lg:p-8 lg:border-r">
+      <article>
+        <h1 class="text-4xl font-black mb-4 leading-none">{{ document.title }}</h1>
+        <nuxt-content :document="document" />
+      </article>
+    </div>
+    <div class="w-full lg:w-1/4 py-4 lg:p-8">
+      <p class="mb-4 text-gray-500 uppercase font-bold text-sm">On this page</p>
+      <nav>
+        <!-- TODO: Toc -->
+        <ul>
+          <li>...</li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      document: {
+        title: 'Title'
+      }
+    }
+  }
+}
+</script>
